432825722fa88dcdc29c2bbd3eb381d003883f7f
Hiep Nguyen <lamchanhiephceet@gmail.com>
2020-09-29 16:53:05 +0700

    security_complete

---
+++ spring-security/src/main/java/com/codegym/SecuriryWebApplicatonInitializer.java
@@ -0,0 +1,6 @@
+package com.codegym;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecuriryWebApplicatonInitializer extends AbstractSecurityWebApplicationInitializer {
+}
