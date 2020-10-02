d42ca98735dcae3256a4d130b5379a9e7f45434e
Hiep Nguyen <lamchanhiephceet@gmail.com>
2020-10-02 14:21:52 +0700

    spring-boot-convert

---
+++ spring-boot-cms-validation/src/main/java/com/codegym/cms/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.codegym.cms;
+
+import com.codegym.cms.service.CustomerService;
+import com.codegym.cms.service.impl.SimpleCustomerServiceImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Bean
+	public CustomerService customerService(){
+		return new SimpleCustomerServiceImpl();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-cms-validation/src/main/java/com/codegym/cms/service/CustomerServiceFactory.java
@@ -0,0 +1,15 @@
+package com.codegym.cms.service;
+
+import com.codegym.cms.service.impl.SimpleCustomerServiceImpl;
+
+
+public class CustomerServiceFactory {
+    private static CustomerService singleton;
+
+    public static synchronized CustomerService getInstance() {
+        if (singleton == null) {
+            singleton = new SimpleCustomerServiceImpl();
+        }
+        return singleton;
+    }
+}
